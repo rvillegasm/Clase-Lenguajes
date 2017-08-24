ba33db42005ffc49bb7caf9ea8e07f1e8e054279
rvillegasm <rvillegasm@f3661382-f94e-4467-94a3-fe5fc129ac9e>
2017-08-24 22:45:27 +0000

    Cambios punto 4

--- parciales/parcial01/punto04/Main.java
+++ parciales/parcial01/punto04/Main.java
@@ -2,6 +2,34 @@ public class Main {
 
    private static boolean evaluar(LogicaAbstracta arbol) {
       boolean ret = false;
+
+      if(arbol == null) {
+	  ret = false;
+      }
+      else if(arbol instanceof Verdadero) {
+	  Verdadero unVer = (Verdadero) arbol;
+	  ret = unVer.obtValor();
+      }
+      else if(arbol instanceof Falso) {
+	  Falso unFal = (Falso) arbol;
+	  ret = unFal.obtValor();
+      }
+      else {
+	  Binario unBin = (Binario) arbol;
+
+	  switch (unBin.obtOper()) {
+	  case Y:
+	      ret = evaluar(unBin.obtIzq()) &&
+		  evaluar(unBin.obtDer());
+	      break;
+
+	  case C:
+	      ret = !evaluar(unBin.obtIzq()) ||
+		  evaluar(unBin.obtDer());
+	      break;
+	  }
+      }
+      
       return ret;
    }
 
