1a0924a63136057ded3d95a17b519fab62b3d0fd
rvillegasm <rvillegasm@f3661382-f94e-4467-94a3-fe5fc129ac9e>
2017-09-07 14:04:23 +0000

    programas en c++

---
+++ clases/clase14/global01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int x;
+bool t = true;
+const signed short int y = 12;
+
+//funcion principal
+int main(void) {
+  std::cout << "x: " << x << " y: " << y
+	    << " t: " << t << std::endl;
+  return 0;
+}
---
+++ clases/clase14/local01.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+//funcion principal
+int main(void) {
+  int x;
+  bool t = true;
+  const signed short int y = 12;
+  
+  std::cout << "x: " << x << " y: " << y
+	    << " t: " << t << std::endl;
+  return 0;
+}
