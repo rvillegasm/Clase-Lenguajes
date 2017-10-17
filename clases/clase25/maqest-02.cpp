14bc41df400db34f9ac0e12c3489bb139887fa29
rvillegasm <rvillegasm@f3661382-f94e-4467-94a3-fe5fc129ac9e>
2017-10-17 13:57:37 +0000

    ejemplos maquinas de estado

---
+++ clases/clase25/maqest-02.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int
+main() {
+
+  enum States { Start, Ids };
+
+  States cs = Start;
+  int cc;
+  ostringstream oss;
+  int nIds= 0;
+  int nCharsIds = 0;
+  ifstream in("input-01.txt");
+
+  while ((cc = in.get()) != EOF) {
+    switch (cs) {
+    case Start:
+      if (isalpha(cc)) {
+        cs = Ids;
+        oss << (char) cc;
+      }
+      break;
+
+    case Ids:
+      if (isalnum(cc)) {
+        cs = Ids;
+        oss << (char) cc;
+      }
+      else {
+        cout << oss.str() << endl;
+        nIds++;
+        nCharsIds += oss.str().size();
+        cs = Start;
+        oss.str("");
+        oss.clear();
+      }
+    }
+  }
+
+  cout << "*** Resumen ***" << endl;
+  cout << "---------------" << endl;
+  cout << "number ids: " << nIds << endl;
+  cout << "number char ids: " << nCharsIds << endl;
+
+  return EXIT_SUCCESS;
+}
