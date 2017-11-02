06aaf3faed8bb6b09bd605b74863faf06f752d83
rvillegasm <rvillegasm@f3661382-f94e-4467-94a3-fe5fc129ac9e>
2017-11-02 13:45:57 +0000

    punto 3

---
+++ parciales/parcial03/smb/smb.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main(void) {
+
+  int i, j, local, visitante;
+  
+  while(cin >> i >> j) {
+    cin >> local >> visitante;
+    i += visitante;
+    j += local;
+  }
+
+  if(i > j) {
+    cout << "ganó visitante" << endl;
+  }
+  else if(j > i) {
+    cout << "ganó local"<< endl;;
+  }
+  else{
+    cout << "Hubo un empate" << endl;
+  }
+
+  return 0;
+}
